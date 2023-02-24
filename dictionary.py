efa49247108f95a75906daf63e2883fdbfbffaa5
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-24 16:22:38 +0530

    try dictionary type

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+ic={'name':'red','age':10}
+print(dic) #will output all the key-value pairs. {'name':'red','age':10}
+print(dic['name']) #will output only value with 'name' key. 'red'
+print(dic.values()) #will output list of values in dic. ['red',10]
+print(dic.keys()) 
