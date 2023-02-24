9dd7d44119efa22c9c0b459ee60e998d83e6e4aa
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-24 16:07:15 +0530

    try string data type

---
+++ string data.py
@@ -0,0 +1,4 @@
+a_str = 'Hello World'
+print(a_str) #output will be whole string. Hello World
+print(a_str[0]) #output will be first character. H
+print(a_str[0:5]) #output will be first five characters. Hell
